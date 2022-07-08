a5bf870b638c4ca07e3e87b812aa3902472d5303
livingchaoss <101403441+livingchaoss@users.noreply.github.com>
2022-07-08 05:37:47 -0700

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -7,20 +7,20 @@
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
- * @perv: points to the previous node
+ * @prev: points to the previous node
  * @next: points to the next node
- * 
+ *
  * Description: doubly linked list node structure
  * for alx project
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
-}dlistint_t;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-size_t Print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
