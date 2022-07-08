693893eae7242ab4de349698064eb398274c8ae6
livingchaoss <101403441+livingchaoss@users.noreply.github.com>
2022-07-08 05:33:30 -0700

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,17 +8,17 @@
  */
 size_t Print_dlistint(const dlistint_t *h)
 {
-    size_t nodes = 0;
+        size_t nodes = 0;
 
-    if (!h)
-            return (0);
+        if (!h)
+                return (0);
     
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        nodes++;
-    }
+        while (h)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+                nodes++;
+        }
 
-    return (nodes);
+        return (nodes);
 }
